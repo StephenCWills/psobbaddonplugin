d1ab5e35f75be2d6d3756c5f490f9ffb8e69e770
staphen <staphen@gmail.com>
2022-09-30 22:23:29 -0400

    Reset DS4 device after it disconnects

--- bbmod/src/imgui_dinput8_dev.cpp
+++ bbmod/src/imgui_dinput8_dev.cpp
@@ -27,6 +27,24 @@ STDMETHODIMP ImguiDInput::CreateDevice(REFGUID guid, LPDIRECTINPUTDEVICE8A * dev
     if (guid == GUID_SysMouse) {
         *devptr = (LPDIRECTINPUTDEVICE8A) new ImguiDInputDevice(*devptr, false);
     }
+
+    bool match =
+        guid.Data1 == 0x9E573EDF &&
+        guid.Data2 == 0x7734 &&
+        guid.Data3 == 0x11D2 &&
+        guid.Data4[0] == 0x8D &&
+        guid.Data4[1] == 0x4A &&
+        guid.Data4[2] == 0x23 &&
+        guid.Data4[3] == 0x90 &&
+        guid.Data4[4] == 0x3F &&
+        guid.Data4[5] == 0xB6 &&
+        guid.Data4[6] == 0xBD &&
+        guid.Data4[7] == 0xF7;
+
+    if (match) {
+        *devptr = (LPDIRECTINPUTDEVICE8A) new ImguiDInputDeviceDS4(guid, inner, *devptr);
+    }
+
     return ret;
 }
 
@@ -262,5 +280,292 @@ STDMETHODIMP ImguiDInputDevice::SetActionMap(LPDIACTIONFORMATA arg1, LPCSTR arg2
 
 STDMETHODIMP ImguiDInputDevice::GetImageInfo(LPDIDEVICEIMAGEINFOHEADERA arg1)
 {
+    return device->GetImageInfo(arg1);
+}
+
+
+// -----------------------------------------------------------------------
+
+
+STDMETHODIMP ImguiDInputDeviceDS4::QueryInterface(REFIID riid, LPVOID* ppvObj)
+{
+    if (device == nullptr)
+        return -2147220983;
+
+    return device->QueryInterface(riid, ppvObj);
+}
+
+STDMETHODIMP_(ULONG) ImguiDInputDeviceDS4::AddRef(void)
+{
+    if (device == nullptr)
+        return -2147220983;
+
+    return device->AddRef();
+}
+
+STDMETHODIMP_(ULONG) ImguiDInputDeviceDS4::Release(void)
+{
+    if (device == nullptr)
+        return -2147220983;
+
+    return device->Release();
+}
+
+STDMETHODIMP ImguiDInputDeviceDS4::GetCapabilities(LPDIDEVCAPS caps)
+{
+    if (device == nullptr)
+        return -2147220983;
+
+    return device->GetCapabilities(caps);
+}
+
+STDMETHODIMP ImguiDInputDeviceDS4::EnumObjects(LPDIENUMDEVICEOBJECTSCALLBACKA arg1, LPVOID arg2, DWORD arg3)
+{
+    if (device == nullptr)
+        return -2147220983;
+
+    return device->EnumObjects(arg1, arg2, arg3);
+}
+
+STDMETHODIMP ImguiDInputDeviceDS4::GetProperty(REFGUID arg1, LPDIPROPHEADER arg2)
+{
+    if (device == nullptr)
+        return -2147220983;
+
+    return device->GetProperty(arg1, arg2);
+}
+
+STDMETHODIMP ImguiDInputDeviceDS4::SetProperty(REFGUID arg1, LPCDIPROPHEADER arg2)
+{
+    if (device == nullptr)
+        return -2147220983;
+
+    return device->SetProperty(arg1, arg2);
+}
+
+STDMETHODIMP ImguiDInputDeviceDS4::Acquire(void)
+{
+    if (device == nullptr)
+        return -2147220983;
+
+    return device->Acquire();
+}
+
+STDMETHODIMP ImguiDInputDeviceDS4::Unacquire(void)
+{
+    if (device == nullptr)
+        return -2147220983;
+
+    return device->Unacquire();
+}
+
+STDMETHODIMP ImguiDInputDeviceDS4::GetDeviceState(DWORD cbData, LPVOID lpvData)
+{
+    if (device == nullptr)
+        return -2147220983;
+
+    return device->GetDeviceState(cbData, lpvData);
+}
+
+STDMETHODIMP ImguiDInputDeviceDS4::GetDeviceData(DWORD arg1, LPDIDEVICEOBJECTDATA arg2, LPDWORD arg3, DWORD arg4)
+{
+    if (device == nullptr)
+        return -2147220983;
+
+    return device->GetDeviceData(arg1, arg2, arg3, arg4);
+}
+
+STDMETHODIMP ImguiDInputDeviceDS4::SetDataFormat(LPCDIDATAFORMAT arg1)
+{
+    if (device == nullptr)
+        return -2147220983;
+
+    dataFormat = arg1;
+    return device->SetDataFormat(arg1);
+}
+
+STDMETHODIMP ImguiDInputDeviceDS4::SetEventNotification(HANDLE arg1)
+{
+    if (device == nullptr)
+        return -2147220983;
+
+    return device->SetEventNotification(arg1);
+}
+
+STDMETHODIMP ImguiDInputDeviceDS4::SetCooperativeLevel(HWND arg1, DWORD arg2)
+{
+    if (device == nullptr)
+        return -2147220983;
+
+    cooperativeLevelWnd = arg1;
+    cooperativeLevelWord = arg2;
+    return device->SetCooperativeLevel(arg1, arg2);
+}
+
+STDMETHODIMP ImguiDInputDeviceDS4::GetObjectInfo(LPDIDEVICEOBJECTINSTANCEA arg1, DWORD arg2, DWORD arg3)
+{
+    if (device == nullptr)
+        return -2147220983;
+
+    return device->GetObjectInfo(arg1, arg2, arg3);
+}
+
+STDMETHODIMP ImguiDInputDeviceDS4::GetDeviceInfo(LPDIDEVICEINSTANCEA arg1)
+{
+    if (device == nullptr)
+        return -2147220983;
+
+    return device->GetDeviceInfo(arg1);
+}
+
+STDMETHODIMP ImguiDInputDeviceDS4::RunControlPanel(HWND arg1, DWORD arg2)
+{
+    if (device == nullptr)
+        return -2147220983;
+
+    return device->RunControlPanel(arg1, arg2);
+}
+
+STDMETHODIMP ImguiDInputDeviceDS4::Initialize(HINSTANCE arg1, DWORD arg2, REFGUID arg3)
+{
+    if (device == nullptr)
+        return -2147220983;
+
+    return device->Initialize(arg1, arg2, arg3);
+}
+
+STDMETHODIMP ImguiDInputDeviceDS4::CreateEffect(REFGUID arg1, LPCDIEFFECT arg2, LPDIRECTINPUTEFFECT* arg3, LPUNKNOWN arg4)
+{
+    if (device == nullptr)
+        return -2147220983;
+
+    return device->CreateEffect(arg1, arg2, arg3, arg4);
+}
+
+STDMETHODIMP ImguiDInputDeviceDS4::EnumEffects(LPDIENUMEFFECTSCALLBACKA arg1, LPVOID arg2, DWORD arg3)
+{
+    if (device == nullptr)
+        return -2147220983;
+
+    return device->EnumEffects(arg1, arg2, arg3);
+}
+
+STDMETHODIMP ImguiDInputDeviceDS4::GetEffectInfo(LPDIEFFECTINFOA arg1, REFGUID arg2)
+{
+    if (device == nullptr)
+        return -2147220983;
+
+    return device->GetEffectInfo(arg1, arg2);
+}
+
+STDMETHODIMP ImguiDInputDeviceDS4::GetForceFeedbackState(LPDWORD arg1)
+{
+    if (device == nullptr)
+        return -2147220983;
+
+    return device->GetForceFeedbackState(arg1);
+}
+
+STDMETHODIMP ImguiDInputDeviceDS4::SendForceFeedbackCommand(DWORD arg1)
+{
+    if (device == nullptr)
+        return -2147220983;
+
+    return device->SendForceFeedbackCommand(arg1);
+}
+
+STDMETHODIMP ImguiDInputDeviceDS4::EnumCreatedEffectObjects(LPDIENUMCREATEDEFFECTOBJECTSCALLBACK arg1, LPVOID arg2, DWORD arg3)
+{
+    if (device == nullptr)
+        return -2147220983;
+
+    return device->EnumCreatedEffectObjects(arg1, arg2, arg3);
+}
+
+STDMETHODIMP ImguiDInputDeviceDS4::Escape(LPDIEFFESCAPE arg1)
+{
+    if (device == nullptr)
+        return -2147220983;
+
+    return device->Escape(arg1);
+}
+
+STDMETHODIMP ImguiDInputDeviceDS4::Poll(void)
+{
+    auto ret = -2147220983;
+
+    if (device != nullptr) {
+        ret = device->Poll();
+
+        if (ret == -2147220983) {
+            device->Unacquire();
+            device->Release();
+            device = nullptr;
+        }
+    }
+
+    if (device == nullptr) {
+        LPDIRECTINPUTDEVICE8A newDevice;
+        auto devret = dinput->CreateDevice(deviceGuid, &newDevice, nullptr);
+
+        if (devret == DI_OK)
+        {
+            newDevice->AddRef();
+            newDevice->SetDataFormat(dataFormat);
+            newDevice->SetCooperativeLevel(cooperativeLevelWnd, cooperativeLevelWord);
+            newDevice->Acquire();
+            device = newDevice;
+            ret = device->Poll();
+        }
+    }
+
+    return ret;
+}
+
+STDMETHODIMP ImguiDInputDeviceDS4::SendDeviceData(DWORD arg1, LPCDIDEVICEOBJECTDATA arg2, LPDWORD arg3, DWORD arg4)
+{
+    if (device == nullptr)
+        return -2147220983;
+
+    return device->SendDeviceData(arg1, arg2, arg3, arg4);
+}
+
+STDMETHODIMP ImguiDInputDeviceDS4::EnumEffectsInFile(LPCSTR arg1, LPDIENUMEFFECTSINFILECALLBACK arg2, LPVOID arg3, DWORD arg4)
+{
+    if (device == nullptr)
+        return -2147220983;
+
+    return device->EnumEffectsInFile(arg1, arg2, arg3, arg4);
+}
+
+STDMETHODIMP ImguiDInputDeviceDS4::WriteEffectToFile(LPCSTR arg1, DWORD arg2, LPDIFILEEFFECT arg3, DWORD arg4)
+{
+    if (device == nullptr)
+        return -2147220983;
+
+    return device->WriteEffectToFile(arg1, arg2, arg3, arg4);
+}
+
+STDMETHODIMP ImguiDInputDeviceDS4::BuildActionMap(LPDIACTIONFORMATA arg1, LPCSTR arg2, DWORD arg3)
+{
+    if (device == nullptr)
+        return -2147220983;
+
+    return device->BuildActionMap(arg1, arg2, arg3);
+}
+
+STDMETHODIMP ImguiDInputDeviceDS4::SetActionMap(LPDIACTIONFORMATA arg1, LPCSTR arg2, DWORD arg3)
+{
+    if (device == nullptr)
+        return -2147220983;
+
+    return device->SetActionMap(arg1, arg2, arg3);
+}
+
+STDMETHODIMP ImguiDInputDeviceDS4::GetImageInfo(LPDIDEVICEIMAGEINFOHEADERA arg1)
+{
+    if (device == nullptr)
+        return -2147220983;
+
     return device->GetImageInfo(arg1);
 }
